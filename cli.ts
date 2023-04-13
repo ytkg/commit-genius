dfe9dd71df1e94469501c379b0413750f4382119
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-04-13 22:56:18 +0900

    Implement an OpenAI-powered commit message generator

--- cli.ts
+++ cli.ts
@@ -1,10 +1,21 @@
 import { Command } from "./deps.ts"
+import { getDiffText } from "./lib/get_diff_text.ts";
+import { getCommitMessageSuggestion } from "./lib/get_commit_message_suggestion.ts";
 import { VERSION } from "./version.ts"
 
 new Command()
   .name("cg")
   .version(`v${VERSION}`)
-  .action(() => {
-    console.log("Hello, Commit Genius!")
+  .env("OPENAI_ACCESS_TOKEN=<value:string>", "OPENAI ACCESS TOKEN", {
+    required: true,
+  })
+  .action(async (options) => {
+    const diffText = await getDiffText();
+    const commitMessageSuggestion = await getCommitMessageSuggestion(
+      options.openaiAccessToken,
+      diffText,
+    );
+
+    console.log(commitMessageSuggestion);
   })
   .parse(Deno.args);
