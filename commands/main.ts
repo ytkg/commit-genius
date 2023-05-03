0dd4ed34731a6bc8dae3098b582b4281bc9d0fae
swfz <sawafuji.09@gmail.com>
2023-05-04 02:49:48 +0900

    Add support for selecting model type (#16)
    * feat: Add support for GPT-4 and Model Type option
    
    * test: Update help command with model option in main_test.ts
    
    * Update commands/main.ts
    
    Co-authored-by: YOSHIKI <yoshiki.tkg@gmail.com>
    
    * chore: Remove unnecessary variable declarations
    
    * revert
    
    ---------
    
    Co-authored-by: YOSHIKI <yoshiki.tkg@gmail.com>

--- commands/main.ts
+++ commands/main.ts
@@ -1,13 +1,20 @@
-import { Command, ValidationError } from "../deps.ts";
+import { Command, EnumType, ValidationError } from "../deps.ts";
 import { getDiffText } from "../lib/get_diff_text.ts";
 import { getCommitMessageSuggestion } from "../lib/get_commit_message_suggestion.ts";
 
 export class MainCommand extends Command {
   constructor() {
     super();
+
+    const modelType = new EnumType(["gpt-3.5-turbo", "gpt-4"]);
+
     this.name("cg")
       .env("OPENAI_ACCESS_TOKEN=<value:string>", "OPENAI ACCESS TOKEN")
       .env("OPENAI_API_KEY=<value:string>", "OPENAI API KEY")
+      .type("model-type", modelType)
+      .option("-m --model <model:model-type>", "Model Type", {
+        default: "gpt-3.5-turbo" as const,
+      })
       .action(async (options) => {
         const apiKey = options.openaiAccessToken || options.openaiApiKey;
 
@@ -21,6 +28,7 @@ export class MainCommand extends Command {
         const commitMessageSuggestion = await getCommitMessageSuggestion(
           apiKey,
           diffText,
+          options.model,
         );
 
         console.log(commitMessageSuggestion);
