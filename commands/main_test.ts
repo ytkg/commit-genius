d0042b1838385eb26515040c93748d265301bb5e
swfz <sawafuji.09@gmail.com>
2023-05-04 02:21:35 +0900

    test: Update help command with model option in main_test.ts

--- commands/main_test.ts
+++ commands/main_test.ts
@@ -11,6 +11,10 @@ Deno.test("help command", () => {
     `
   Usage: cg
 
+  Options:
+
+    -m, --model  <model>  - Model Type  (Default: "gpt-3.5-turbo", Values: "gpt-3.5-turbo", "gpt-4")
+
   Environment variables:
 
     OPENAI_ACCESS_TOKEN  <value>  - OPENAI ACCESS TOKEN  
