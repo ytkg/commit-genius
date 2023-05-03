517b3f69e3a78d3dc909029caeb8bf4fd4dd3a47
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2023-05-03 22:58:26 +0900

    Add MainCommand class to handle cg command logic

--- cg.ts
+++ cg.ts
@@ -1,31 +1,6 @@
-import { Command, ValidationError } from "./deps.ts";
-import { getDiffText } from "./lib/get_diff_text.ts";
-import { getCommitMessageSuggestion } from "./lib/get_commit_message_suggestion.ts";
+import { MainCommand } from "./commands/main.ts";
 import { VERSION } from "./version.ts";
 
-new Command()
-  .name("cg")
+new MainCommand()
   .version(`v${VERSION}`)
-  .env("OPENAI_ACCESS_TOKEN=<value:string>", "OPENAI ACCESS TOKEN")
-  .env("OPENAI_API_KEY=<value:string>", "OPENAI API KEY")
-  .action(async (options) => {
-    if (
-      options.openaiAccessToken === undefined &&
-      options.openaiApiKey === undefined
-    ) {
-      throw new ValidationError(
-        "Any of the environment variables are required",
-      );
-    }
-
-    const apiKey = options.openaiAccessToken || options.openaiApiKey;
-
-    const diffText = await getDiffText();
-    const commitMessageSuggestion = await getCommitMessageSuggestion(
-      apiKey,
-      diffText,
-    );
-
-    console.log(commitMessageSuggestion);
-  })
   .parse(Deno.args);
