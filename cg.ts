992de0ea811fe1b4a0e697d37f06c18499f55fed
swfz <sawafuji.09@gmail.com>
2023-05-04 02:07:06 +0900

    feat: Add support for selecting model type

--- cg.ts
+++ cg.ts
@@ -1,13 +1,19 @@
-import { Command, ValidationError } from "./deps.ts";
+import { Command, EnumType, ValidationError } from "./deps.ts";
 import { getDiffText } from "./lib/get_diff_text.ts";
 import { getCommitMessageSuggestion } from "./lib/get_commit_message_suggestion.ts";
 import { VERSION } from "./version.ts";
+
+const modelType = new EnumType(["gpt-3.5-turbo", "gpt-4"]);
 
 new Command()
   .name("cg")
   .version(`v${VERSION}`)
   .env("OPENAI_ACCESS_TOKEN=<value:string>", "OPENAI ACCESS TOKEN")
   .env("OPENAI_API_KEY=<value:string>", "OPENAI API KEY")
+  .type("model-type", modelType)
+  .option("-m --model <model:model-type>", "Model Type", {
+    default: "gpt-3.5-turbo" as const,
+  })
   .action(async (options) => {
     if (
       options.openaiAccessToken === undefined &&
@@ -17,13 +23,13 @@ new Command()
         "Any of the environment variables are required",
       );
     }
-
     const apiKey = options.openaiAccessToken || options.openaiApiKey;
 
     const diffText = await getDiffText();
     const commitMessageSuggestion = await getCommitMessageSuggestion(
       apiKey,
       diffText,
+      options.model,
     );
 
     console.log(commitMessageSuggestion);
